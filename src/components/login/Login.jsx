a904cd689e475bbe482a3276e303efec0c164af3
zahid072 <jahidhasan19368@gmail.com>
2024-03-30 16:53:16 +0600

    add gitHub logIn methods

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import {
+  GithubAuthProvider,
   GoogleAuthProvider,
   getAuth,
   signInWithPopup,
@@ -9,10 +10,23 @@ import app from "../../firebase/firebase.init";
 const Login = () => {
   const [user, setUser] = useState(null);
   const auth = getAuth(app);
-  const provider = new GoogleAuthProvider();
-  console.log(app);
+  const googleProvider = new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
+  // signIn with google
   const signInWithGoogle = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log("error:", error.message);
+      });
+  };
+  // signIn with git hub
+  const signInWithGitHub = () => {
+    signInWithPopup(auth, gitHubProvider)
       .then((result) => {
         const user = result.user;
         console.log(user);
@@ -32,22 +46,41 @@ const Login = () => {
         console.log("Error:", error.message);
       });
   };
+
   return (
     <div>
-      {!user && <button
-        onClick={signInWithGoogle}
-        className="px-4 py-2 bg-orange-400 rounded-lg"
-      >
-        Login
-      </button>}
-      {user && <button
-        onClick={handleLogOut}
-        className="px-4 py-2 bg-orange-400 rounded-lg"
-      >
-        Log out
-      </button>}
+      {!user && (
+        <>
+          <button
+            onClick={signInWithGoogle}
+            className="px-4 py-2 bg-orange-400 rounded-lg"
+          >
+            Login
+          </button>
+          <button
+            onClick={signInWithGitHub}
+            className="px-4 py-2 bg-blue-400 rounded-lg"
+          >
+            SignIn with GitHub
+          </button>
+        </>
+      )}
 
-      {user && <h1>User: {user.displayName}</h1>}
+      {user && (
+        <button
+          onClick={handleLogOut}
+          className="px-4 py-2 bg-orange-400 rounded-lg"
+        >
+          Log out
+        </button>
+      )}
+
+      {user && (
+        <>
+          <h1>User: {user.displayName}</h1>
+          <img src={user.photoURL} alt="" />
+        </>
+      )}
     </div>
   );
 };
